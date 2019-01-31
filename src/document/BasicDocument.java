970636c720b0254d748a2b2fca9ab8d78d3fda5a
Ayush Verma <ayush.k1998@gmail.com>
2019-01-31 23:14:55 +0530

    copy numSyllables and countSyllables method

--- src/document/BasicDocument.java
+++ src/document/BasicDocument.java
@@ -84,7 +84,28 @@ public class BasicDocument extends Document
 		// expression for the syllable counting.  We recommend you implement 
 		// the helper function countSyllables in Document.java using a loop, 
 		// and then call it here on each word.
-        return 0;
+		
+		// We provide for you two solutions: One that uses multiple 
+		// regexs to calculate the number of syllables and the other
+		// that finds words using a loop.  The regex solution is commented 
+		// out here at the top.
+
+		/* Our solution using regex's.  Uncoment here to run it*/
+		/*
+		List<String> tokens = getTokens("[aeiouyAEIOUY]+");
+		List<String> loneEs = getTokens("[^aeiouyAEIOUY]+[eE]\\b");
+		List<String> singleEs = getTokens("\\b[^aeiouyAEIOUY]*[eE]\\b");			
+		return tokens.size() - (loneEs.size() - singleEs.size());
+		*/
+				
+		/* Our solution that does NOT use regexs to find syllables */
+		List<String> tokens = getTokens("[a-zA-Z]+");
+		int totalSyllables = 0;
+		for (String word : tokens)
+		{
+			totalSyllables += countSyllables(word);
+		}
+		return totalSyllables;
 	}
 	
 	
@@ -98,22 +119,22 @@ public class BasicDocument extends Document
 		 * in the string, respectively.  You can use these examples to help clarify 
 		 * your understanding of how to count syllables, words, and sentences.
 		 */
-//		testCase(new BasicDocument("This is a test.  How many???  "
-//		        + "Senteeeeeeeeeences are here... there should be 5!  Right?"),
-//				16, 13, 5);
-//		testCase(new BasicDocument(""), 0, 0, 0);
-//		testCase(new BasicDocument("sentence, with, lots, of, commas.!  "
-//		        + "(And some poaren)).  The output is: 7.5."), 15, 11, 4);
-//		testCase(new BasicDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2);
-//		testCase(new BasicDocument("Here is a series of test sentences. Your program should "
-//				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
-//				+ "the correct amount of syllables (example, for example), "
-//				+ "but most of them will."), 49, 33, 3);
-//		testCase(new BasicDocument("Segue"), 2, 1, 1);
-//		testCase(new BasicDocument("Sentence"), 2, 1, 1);
-//		testCase(new BasicDocument("Sentences?!"), 3, 1, 1);
-//		testCase(new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
-//		         32, 15, 1);
+		testCase(new BasicDocument("This is a test.  How many???  "
+		        + "Senteeeeeeeeeences are here... there should be 5!  Right?"),
+				16, 13, 5);
+		testCase(new BasicDocument(""), 0, 0, 0);
+		testCase(new BasicDocument("sentence, with, lots, of, commas.!  "
+		        + "(And some poaren)).  The output is: 7.5."), 15, 11, 4);
+		testCase(new BasicDocument("many???  Senteeeeeeeeeences are"), 6, 3, 2);
+		testCase(new BasicDocument("Here is a series of test sentences. Your program should "
+				+ "find 3 sentences, 33 words, and 49 syllables. Not every word will have "
+				+ "the correct amount of syllables (example, for example), "
+				+ "but most of them will."), 49, 33, 3);
+		testCase(new BasicDocument("Segue"), 2, 1, 1);
+		testCase(new BasicDocument("Sentence"), 2, 1, 1);
+		testCase(new BasicDocument("Sentences?!"), 3, 1, 1);
+		testCase(new BasicDocument("Lorem ipsum dolor sit amet, qui ex choro quodsi moderatius, nam dolores explicari forensibus ad."),
+		         32, 15, 1);
 //		BasicDocument bd = new BasicDocument("Hello World! How are you?");
 //		System.out.println(bd.getTokens("[A-z]+")); // (Regex to extract words)
 //		System.out.println(bd.getTokens("[^.!?]+")); // (Regex to extract sentences)
